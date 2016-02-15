ff44e924a4f01bd39d4b26a39519bf55dd5e7560
Razvan Pistolea <razvy000@gmail.com>
2016-02-15 09:15:21 +0000

    Add top down design of ANN and Layer

---
+++ ann.py
@@ -0,0 +1,21 @@
+
+
+class ANN:
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def update_weights(self):
+        pass
+
+
+class Layer:
+
+    def __init__(self):
+        pass
